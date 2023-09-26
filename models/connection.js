88993059a5476cfad72c7680d483ed9cccbaf1f5
Himanshu Sharma <103371625+Hsharma41126@users.noreply.github.com>
2023-09-26 17:28:49 +0530

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,57 @@
 var mongoose = require("mongoose");
-var url="mongodb://localhost:27017/Smooz";
+var url = require('../constants/constants');
+const indianCities = require('indian-cities-database');
+
 mongoose.connect(url);
 var db = mongoose.connection;
+
+const cities = indianCities.cities
+const states = [...new Set(cities.map(city => city.state))];
+const cities1 = [...new Set(cities.map(city => city.city))];
+// console.log('Available states:', states);
+// console.log('Available cities:', cities1);
+const User = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other"]
+    },
+    phone: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                return /^[0-9]{10}$/.test(value);
+            },
+            message: 'Phone number must be 10 digits long.'
+        },
+        required: true
+    },
+    state: {
+        type: String,
+        enum: states
+    },
+    city: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                return cities1.some(city => city.name === value);
+            },
+            message: 'Invalid city.'
+        }
+    },
+
+});
+
 console.log("Successfully connected to mongodb database...");
-module.exports=db;
+module.exports = { db, User };
