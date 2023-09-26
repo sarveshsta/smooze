9172843f9c5b7c76e77ae5912948ae90262bd603
Himanshu Sharma <103371625+Hsharma41126@users.noreply.github.com>
2023-09-26 17:45:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var registerRouter = require('./routes/register');
+var loginRouter = require('./routes/login');
+var logoutRouter = require('./routes/logout');
+var deactivateRouter = require('./routes/deactivate');
 
 var app = express();
 
@@ -19,8 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+app.use('/deactivate', deactivateRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
