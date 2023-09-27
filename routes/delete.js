1171ff1774b84fc37701c5d8ef3b58acd38a0a8c
Hsharma41126 <himanshu.sharma2020@sait.ac.in>
2023-09-27 15:00:24 +0530

    update for delete user

--- routes/delete.js
+++ routes/delete.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var indexmodel = require('../models/indexmodel');
 
-router.post('/', (req, res, next) => {
+router.delete('/', (req, res, next) => {
   indexmodel.deleteuser(req.body, (result) => {
     if (result) {
       console.log('User deleted successfully.');
