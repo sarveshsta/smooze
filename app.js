4df1c75bd1d0201969d23f7453cf03753753979b
Hsharma41126 <himanshu.sharma2020@sait.ac.in>
2023-11-29 15:38:37 +0530

    updated code

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.use(expressWinston.errorLogger({
 
 
 // // view engine setup
-app.set('views', path.join(__dirname, 'FrontEndTest'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
